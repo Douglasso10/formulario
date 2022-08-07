8954a5e08548720c9b1e13ae860d6da03d7d197e
Douglasso10 <84336458+Douglasso10@users.noreply.github.com>
2022-08-07 13:25:13 -0300

    trabalhando com Javascript

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
-const form = document.getElementById('form')
-const usarname = document.getElementById('usarname')
-const email = document.getElementById('email')
-const passoward = document.getElementById('passoword')
-const passowardConfimation = document.getElementById('passoward-confirm')
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
+const form = document.getElementById("form");
+const username = document.getElementById("usarname");
+const email = document.getElementById("email");
+const passoward = document.getElementById("passoword");
+const passowardConfimation = document.getElementById("passoward-confirm");
 
-    checkInputs()
+form.addEventListener('submit', (e) => {
+   e.preventDefault();
+
+    checkInputs();
 });
 
-function checkInputs (){
-    const usarnameValue = usarname.value;
+function checkInputs() {
+    const usernameValue = username.value;
     const emailValue = email.value;
     const passowardValue = passoward.value;
     const passowordConfimationValue = passowardConfimation.value;
 
-    if (usarnameValue === ''){
-        setErrorFor(usarname, "O nome do usuário é obrigatório.");
+    if (usernameValue === ""){
+        setErrorFor(username, "O nome do usuário é obrigatório.");
     }
 }
 
@@ -27,10 +28,10 @@ function setErrorFor(input, message){
 
     small.innerText = message;
 
-    formControl.className = 'form-control error' 
+    formControl.className = "form_control error" 
 }
 
-function setSuccess (input) {
+function setSuccessFor (input) {
     const formControl = input.parentElement;
     formControl.className = 'form-control success'
 }
